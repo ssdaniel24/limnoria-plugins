3782b1bfca4aab9091b5635faaebccf5bef4a063
ssdaniel24 <bo7oaonteg2m@mail.ru>
2024-12-02 17:37:31 +0300

    RusFun: added horo tests

--- RusFun/test.py
+++ RusFun/test.py
@@ -4,5 +4,11 @@ from supybot.test import *
 class RusFunTestCase(PluginTestCase):
     plugins = ('RusFun',)
 
+    def test_horo(self):
+        self.assertNotError('horo Овен')
+        self.assertNotError('horo дева')
+        self.assertNotError('horo водолей')
+        self.assertNotError('horo Рыбы')
+
 
 # vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
