3d25c26cbeb5bed3a6f1ae660aba2c0fee0e68a4
ssdaniel24 <bo7oaonteg2m@mail.ru>
2024-12-02 15:57:44 +0300

    RateCBR: added tests

--- RateCBR/test.py
+++ RateCBR/test.py
@@ -4,5 +4,15 @@ from supybot.test import *
 class RateCBRTestCase(PluginTestCase):
     plugins = ('RateCBR',)
 
+    def testGettingRate(self):
+        self.assertNotError('rate')
+        self.assertNotError('rate EUR')
+        self.assertNotError('rate CNY')
+        self.assertNotError('rate UAH')
+        self.assertNotError('rate BYN')
+
+    def testGettingAvailableCurrencies(self):
+        self.assertNotError('currencies')
+
 
 # vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
