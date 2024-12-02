239bc0f3abadc0251c538dd88c624899e87de839
ssdaniel24 <bo7oaonteg2m@mail.ru>
2024-12-02 16:01:36 +0300

    IPIinfo: added tests

--- IPInfo/test.py
+++ IPInfo/test.py
@@ -4,5 +4,10 @@ from supybot.test import *
 class IPInfoTestCase(PluginTestCase):
     plugins = ('IPInfo',)
 
+    def test_getting(self):
+        self.assertNotError('get 1.1.1.1')
+        self.assertNotError('get 8.8.8.8')
+        self.assertNotError('get 77.88.8.8')
+
 
 # vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
