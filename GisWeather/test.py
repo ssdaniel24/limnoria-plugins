0fcf9f30e5d8857bfe538eb3de4d3b19742ded00
ssdaniel24 <bo7oaonteg2m@mail.ru>
2024-12-05 21:01:15 +0300

    GisWeather: added some tests

--- GisWeather/test.py
+++ GisWeather/test.py
@@ -4,5 +4,10 @@ from supybot.test import *
 class GisWeatherTestCase(PluginTestCase):
     plugins = ('GisWeather',)
 
+    def test_weather_getting(self):
+        self.assertNotError('weather Moscow')
+        self.assertNotError('weather Minsk')
+        self.assertNotError('weather Oymyakon')
+
 
 # vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
